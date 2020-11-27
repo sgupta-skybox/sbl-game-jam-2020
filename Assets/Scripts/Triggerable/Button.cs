7342bd7f523b92fbde93a510b7d675dc529fcab6
ArtaSeifySkyBox <arta.seify@skyboxlabs.com>
2020-11-27 10:14:00 -0700

    Introduced a button that can open/close a door

---
+++ Assets/Scripts/Triggerable/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : TriggerableBase
+{
+    [SerializeField]
+    public GameObject objectToTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnTriggered() 
+    {
+        objectToTrigger.SetActive(false);
+    }
+
+    protected override void OnUntriggered()
+    {
+        objectToTrigger.SetActive(true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")
+            || other.gameObject.layer == LayerMask.NameToLayer("MovableObject"))
+        {
+            IsTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player")
+            || other.gameObject.layer == LayerMask.NameToLayer("MovableObject"))
+        {
+            IsTriggered = false;
+        }
+    }
+}
