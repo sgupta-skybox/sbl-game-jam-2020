6fa1918346cfc59d2883fd98775a5c78f0469b37
ykim <younggi.kim@skyboxlabs.com>
2020-11-27 10:45:02 -0800

    grab the closest one

---
+++ Assets/Utils/FloatExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class FloatExtension
+{
+    public static bool IsClosed( this float lhs, float rhs, float delta = float.Epsilon )
+    {
+        return Mathf.Abs(lhs - rhs) <= delta;
+    }
+}
