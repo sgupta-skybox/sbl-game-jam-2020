1536840950cd29495be49a3ca26abbebb109f70c
ykim <younggi.kim@skyboxlabs.com>
2020-11-27 16:25:03 -0800

    updated changes from ProjectileSpawner

--- Assets/Scripts/Traps/ProjectileSpawner.cs
+++ Assets/Scripts/Traps/ProjectileSpawner.cs
@@ -8,15 +8,9 @@ public class ProjectileSpawner : MonoBehaviour
     Projectile ProjectilePrefab;
 
     [SerializeField]
-    Vector2 ProjectileDirection;
-
-    Vector2 nextProjectileDirection;
-
-    [SerializeField]
     float SpawnCooldown = 1.0f;
     void Start()
     {
-        nextProjectileDirection = ProjectileDirection;
         StartCoroutine(FireProjectile());
     }
 
@@ -25,19 +19,8 @@ public class ProjectileSpawner : MonoBehaviour
     {
         while( true )
         {
-            if (ProjectileDirection == Vector2.zero)
-                nextProjectileDirection = Random.insideUnitCircle.normalized;
-            else
-                nextProjectileDirection = ProjectileDirection;
-
-            float angle = Mathf.Atan2(nextProjectileDirection.y, nextProjectileDirection.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-            yield return new WaitForSeconds(SpawnCooldown * 0.5f);
-
-            var projectile = Instantiate(ProjectilePrefab, transform.position, Quaternion.identity );
-            projectile.DirProjectile = nextProjectileDirection;
-            yield return new WaitForSeconds(SpawnCooldown * 0.5f);
+            Instantiate(ProjectilePrefab, transform.position, transform.rotation);
+            yield return new WaitForSeconds(SpawnCooldown * 1.0f);
         }
     }
 }
