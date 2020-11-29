3240f022da70b0d453ba0a3079432b4dad78b357
ykim <younggi.kim@skyboxlabs.com>
2020-11-29 12:47:49 -0800

    replaced few doors

---
+++ Assets/Scripts/Level/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    GameObject closedSprite;
+    [SerializeField]
+    GameObject openSprite;
+    Collider2D collider;
+
+    bool isOpened = false;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+    public void OpenDoor( bool open )
+    {
+        isOpened = open;
+        closedSprite.SetActive(!open);
+        openSprite.SetActive(open);
+        collider.enabled = !open;
+    }
+
+    public void ToggleDoor()
+    {
+        OpenDoor(!isOpened);
+    }
+}
