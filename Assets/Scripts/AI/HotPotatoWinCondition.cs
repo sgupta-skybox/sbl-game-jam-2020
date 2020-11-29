5f77976a5d99dc03a7c280582b6b75e74e689e33
sgupta-skybox <shiva.gupta@skyboxlabs.com>
2020-11-29 15:40:27 -0800

    Updated HotPotato level

---
+++ Assets/Scripts/AI/HotPotatoWinCondition.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotPotatoWinCondition : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    public TriggerableBase doorTrigger;
+    public AIBoxThrower enemy;
+
+    bool[] triggeredObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggeredObjects = new bool[gameObjects.Length];
+        for(int i = 0; i < gameObjects.Length; ++i)
+		{
+            if (gameObjects[i].transform.position.x > 0)
+                triggeredObjects[i] = true;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        for (int i = 0; i < gameObjects.Length; ++i)
+        {
+            if (gameObjects[i].transform.position.x > 0)
+                triggeredObjects[i] = true;
+        }
+        int index = System.Array.FindIndex(gameObjects, gameObject=> gameObject == collision.gameObject);
+        if( index >= 0)
+		{
+            triggeredObjects[index] = true;
+        }
+
+        if( System.Array.TrueForAll(triggeredObjects, triggeredObject => triggeredObject))
+		{
+            doorTrigger.IsTriggered = true;
+            enemy.LevelFinish();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+	{
+        int index = System.Array.FindIndex(gameObjects, gameObject => gameObject == collision.gameObject);
+        if (index >= 0)
+        {
+            triggeredObjects[index] = false;
+        }
+    }
+
+
+}
