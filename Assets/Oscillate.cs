b5d6ae78f845a41d970f5efb7b3a0c6b1926861b
Prithiraj Ghosh <prithiraj2005@gmail.com>
2020-11-28 20:07:23 -0500

    throwable box intro level

---
+++ Assets/Oscillate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillate : MonoBehaviour
+{
+    [SerializeField]
+    float MaxYAmplitude = 0.0f;
+
+    [SerializeField]
+    float MaxXAmplitude = 0.0f;
+
+    float currentAngle = 0.0f;
+
+    [SerializeField]
+    float AngleSpeed = 1.0f;
+
+    Vector2 initialPos;
+
+    void Start()
+    {
+        initialPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 currentPos = initialPos;
+        currentAngle += AngleSpeed * Time.fixedDeltaTime;
+
+        if (currentAngle > Mathf.PI)
+        {
+            currentAngle -= Mathf.PI;
+        }
+
+        currentPos.x += Mathf.Sin(currentAngle) * MaxXAmplitude;
+        currentPos.y += Mathf.Sin(currentAngle) * MaxYAmplitude;
+
+        transform.position = currentPos;
+    }
+}
