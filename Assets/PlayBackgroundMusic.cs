c6ff31bbe41f50d5dee7059a71d3d3655dc9f8cd
cero <prithiraj2005@gmail.com>
2020-11-29 20:49:14 -0500

    background music

---
+++ Assets/PlayBackgroundMusic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBackgroundMusic : MonoBehaviour
+{
+    void OnTriggerEnter2D()
+    {
+        GameObject.FindGameObjectWithTag(AudioManager.NAME).GetComponent<AudioManager>().PlayLoop();
+    }
+}
--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,11 @@ public class AudioManager : MonoBehaviour
 {
     public const string NAME = "AudioManager";
     List<AudioClip> clipsPlayedThisFrame;
+    AudioSource source;
     void Awake() // needs to happen before OnEnable is called on other scripts
     {
         DontDestroyOnLoad(gameObject);
+        source = GetComponent<AudioSource>();
         clipsPlayedThisFrame = new List<AudioClip>();
     }
 
@@ -22,7 +24,13 @@ public class AudioManager : MonoBehaviour
         if (!clipsPlayedThisFrame.Contains(audioClip))
         {
             clipsPlayedThisFrame.Add(audioClip);
-            AudioSource.PlayClipAtPoint(audioClip, Vector2.zero, volume);
+            AudioSource.PlayClipAtPoint(audioClip,Vector3.zero, volume);
         }
+    }
+
+    public void PlayLoop(float volume = 0.2f)
+    {
+        source.volume = volume;
+        source.Play();
     }
 }
